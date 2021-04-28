bf6620468546d0004de77d60914b1d00efd6f75e
Lucas Alves <lukkealvess@gmail.com>
2021-04-27 23:46:39 -0300

    Update ex057.py
    Atualização e melhora do código...

--- ex057.py
+++ ex057.py
@@ -1,11 +1,8 @@
 """Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores
 'M' ou 'F'.Caso esteja errado, peça a digitação novamente até ter um valor correto."""
-m = 'M'
-f = 'F'
-while m == 'M' and f == 'F':
-    s = str(input('Qual o seu sexo [M/F]: ').strip()).upper()
-    if s != 'M' and s != 'F':
-        print('\033[1:31mComando invalido, tente novamente!\033[m')
-    else:
-        print('FIM')
+
+s = str(input('Qual o seu sexo [M/F]: ').strip()).upper()[0]
+while s not in 'MF':
+    s = str(input('\033[1:33mDados invalidos, digite novamente\033[m').strip())
+print('Sexo registrado!')
 
