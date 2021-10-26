c014405cebc1fe81d35c10a28aaeb1eed842c2ab
Lucas Alves <ladscg4@gmail.com>
2021-10-26 13:37:21 -0300

    Bigger and Smaller with List

---
+++ ex078.py
@@ -0,0 +1,27 @@
+"""Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor
+valor digitado e as suas respectivas posições na lista."""
+
+
+# lista = []
+# menor = 0
+# maior = 0
+# for v in range(0, 5):
+#     lista.append(int(input(f"Digite um valor para a posição {v}: ")))
+#     if v == 0:
+#         menor = maior = lista[v]
+#     else:
+#         if lista[v] > maior:
+#             maior = lista[v]
+#         if lista[v] < menor:
+#             menor = lista[v]
+# print("-="*30)
+# print(f"Você digitou os valores: {lista}")
+# print(f"O menor valor foi {menor} na posição", end=" ")
+# for i, v in enumerate(lista):
+#     if v == menor:
+#         print(f"{i}...", end="")
+# print(f"\nO maior valor foi {maior} na posição", end=" ")
+# for i, v in enumerate(lista):
+#     if v == maior:
+#         print(f"{i}...", end="")
+
